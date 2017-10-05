1e5a1263c4f67185bc8ebebc447a9149c9e6a1ee
Zakaria <zakariaarsyadm@hotmail.com>
2017-10-06 06:43:06 +0700

    Membuat tabel pasien, dokter, ruang, rawat. Membuat kelas DatabaseConnection untuk menghubungkan project dengan database

---
+++ src/main/java/com/tabeldata/DatabaseConnection.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tabeldata;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+/**
+ *
+ * @author namhee
+ */
+public class DatabaseConnection {
+
+    private final static String url = "jdbc:postgresql://localhost:5432/bootcamp_latihan";
+    private final static String username = "bootcamp_latihan";
+    private final static String password = "latihan";
+
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+    
+    public static void main(String[] args) throws SQLException {
+        DatabaseConnection koneksiDB = new DatabaseConnection();
+        Connection connection = koneksiDB.getDataSource().getConnection();
+        if (connection != null) {
+            System.out.println("Database Connection Success");
+        } else {
+            System.out.println("Database Connection Failed");
+        }
+    }
+
+}
